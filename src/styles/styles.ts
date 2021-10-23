2cd23bf16e25b7f9e12bdc73a1267d377ed32ca5
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-23 12:37:50 +0000

    create the Add new item ccomponent with a useState hook added

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -38,6 +38,15 @@ export const NewItemButton = styled.button`
   text-align: center;
 `
 
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`
+
 export const AppContainer = styled.div`
  align-items: flex-start;
   background-color: #3179ba;

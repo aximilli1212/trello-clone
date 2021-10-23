37b5e26f311e39375624c26391c66423dad8296d
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-23 12:07:46 +0000

    Add newItem form button and its container

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -20,6 +20,23 @@ export const AddItemButton = styled.button<AddItemButtonProps>`
   }
   `
 
+export const NewItemFormContainer = styled.div`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`
+
+export const NewItemButton = styled.button`
+  background-color: #5aac44;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+`
 
 export const AppContainer = styled.div`
  align-items: flex-start;

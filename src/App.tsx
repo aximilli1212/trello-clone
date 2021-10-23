a50fda37f5fef185abd050a8e4d7b559bab8e308
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-23 13:18:16 +0000

    Add scaffold view sample for all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {AddNewItem} from "./components/NewItem/AddNewItem";
 import {AppContainer} from "./styles/styles";
+import {Column} from "./components/Column/Column";
+import {Card} from "./components/Card/Card";
 
 export const App = () =>{
-  return <AppContainer>
-    Columns will go here
-  </AppContainer>;
+    return (
+        <AppContainer>
+        <Column text="To Do">
+            <Card text="Generate app scaffold" />
+        </Column>
+        <Column text="In Progress">
+            <Card text="Learn Typescript" />
+        </Column>
+        <Column text="Done">
+            <Card text="Begin to use static typing" />
+        </Column>
+        <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+        </AppContainer>
+        );
 }
-
-

d66fc0a84f5231940ca35f2c597361eab6f4b075
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-27 15:07:35 +0000

    Alow dispatch of main column list creation

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import {AppContainer} from "./styles/styles";
 import {Column} from "./components/Column/Column";
 import {Card} from "./components/Card/Card";
 import {useAppState} from "./state/AppStateContext";
+import { addList } from "./state/actions"
 
-export const App = () =>{
-    const {lists} = useAppState();
+    export const App = () =>{
+    const {lists, dispatch} = useAppState();
     return (
         <AppContainer>
             {lists.map((list)=>(
                 <Column id={list.id} key={list.id} text={list.text} />
             ))}
-        <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+        <AddNewItem
+            toggleButtonText="+ Add another list"
+            onAdd={text => dispatch(addList(text))}
+            />
         </AppContainer>
         );
 }
